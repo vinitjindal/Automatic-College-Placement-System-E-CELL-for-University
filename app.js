04f573a750955876358d8a83ee066ca361bd5931
Vishal Verma <contact.vishalvermachd@gmail.com>
2018-03-22 10:18:46 +0000

    Added the student support

--- app.js
+++ app.js
@@ -29,12 +29,47 @@ var companySchema = new mongoose.Schema({
 });
 
 var studentSchema = new mongoose.Schema({
-    
+    rollNo:String,
+    firstName:String,
+    midName:String,
+    lastName:String,
+    mobileNo:Number,
+    gender:String,
+    dob:String,
+    fatherName:String,
+    mailId:String,
+    state:String,
+    town:String,
+    houseNo:String,
+    street:String,
+    pinCode:Number,
+        edu10Name:String,
+        edu10Type:String,
+        edu10Board:String,
+        edu10Stream:String,
+        edu10Year:String,
+        edu10CgpaObt:String,
+        edu10CgpaMax:String,
+            edu12Name:String,
+            edu12Type:String,
+            edu12Board:String,
+            edu12Stream:String,
+            edu12Year:String,
+            edu12CgpaObt:String,
+            edu12CgpaMax:String,
+                eduGradName:String,
+                eduGradType:String,
+                eduGradBoard:String,
+                eduGradStream:String,
+                eduGradYear:String,
+                eduGradCgpaObt:String,
+                eduGradCgpaMax:String,
+        gitHub:String,
+        skillTag:String
 });
 
-
-
 var Company = mongoose.model("Company",companySchema);
+var Student = mongoose.model("Student",studentSchema);
 
 // Company.create({
 //     companyName : "armin",
@@ -125,6 +160,23 @@ app.get("/user/admin/adminRecruited/:id",function(req, res) {
 
 app.get("/user/:username/userEdit",function(req,res){
     res.render("userInput");
+})
+
+app.post("/user/:username/userEdit",function(req,res){
+    var user=req.params.student._Id;
+    Student.create(req.body.student,function(err,newStudent){
+        if(err){
+            console.log("error created");
+        }
+        else
+        {
+            console.log("DB created");
+            console.log(newStudent);
+            
+            console.log("$$$$$$$$$ "+user);
+        }
+    });
+    res.redirect("/user/:username/userEdit");
 })
 
 app.get("/user/:username/userView",function(req, res) {
